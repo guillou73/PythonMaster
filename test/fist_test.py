b323ba5cb756d0c21ee8e20fd7dc414edc940cfb
Hiteshkumar Dhamshaniya <dev.hitesh.uk@gmail.com>
2024-12-05 21:47:23 +0000

    More practice on test

--- test/fist_test.py
+++ test/fist_test.py
@@ -1,5 +1,25 @@
 import unittest
 import math
 
-# class TestCalc(unittest.TestCase):
-    # def test_add(self):
+def encrypt(message: str):
+    return message
+
+class TestCalc(unittest.TestCase):
+    def setUp(self):
+        self.my_message = "banana"
+    # some test methods
+
+    def test_message(self):
+        self.assertIsNotNone(self.my_message)
+
+    def test_inputType(self):
+        self.assertIsInstance(self.my_message, str)
+
+    def test_funcRetrunSomething(self):
+        self.assertEqual(encrypt(self.my_message),str)
+    
+    def test_lenIO(self):
+        self.assertEqual(len(self.my_message), len(self.my_message))
+
+if __name__ == "__main__":
+    unittest.main()
