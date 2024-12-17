05de515d42895d846bdea03f70360e8db106e1f5
Hiteshkumar Dhamshaniya <dev.hitesh.uk@gmail.com>
2024-12-17 19:10:24 +0000

    Re

---
+++ src/pyspark/data_trans.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+salesDataCSVPath = "D://Workspace//PythonMaster//src//pyspark//sample_data.csv"
+
+df = pd.read_csv(salesDataCSVPath)
+
+# df.show()
+print(salesDataCSVPath)
+
+print("Hello")
